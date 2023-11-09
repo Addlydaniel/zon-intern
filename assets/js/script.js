20d9fd506384576347dc7255d4a7954acf0475a4
addly <addlydaniel2008@gmail.com>
2023-11-09 15:21:02 +0530

    new

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+document.querySelector(".close").addEventListener("click",function(){
+document.querySelector(".container").style.display="none";
+});
+
+window.addEventListener("load",function(){
+    setTimeout(
+        function open(event){
+            document.querySelector(".container").style.display="block";
+        },
+        1000
+    )
+
+    })
