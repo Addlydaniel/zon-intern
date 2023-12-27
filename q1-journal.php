0a015153a8b592b9749681a6871f97d1f108ebde
Raghulzonduo <raghulzonduo@gmail.com>
2023-12-27 15:52:14 +0530

    commit

--- q1-journal.php
+++ q1-journal.php
@@ -7,26 +7,20 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $name = $_POST["name"];
   $department = $_POST["department"];
   $domain = $_POST["domain"];
-  $topics = $_POST["topics"];
-  $phd = $_POST["phd"];
-  $impact = $_POST["impact"];
+ 
   $email = $_POST["email"];
   $phone = $_POST["phone"];
-  $assistance = $_POST["assistance"];
-
+ 
   // Compose email
   $adminEmail = "zonduotechnology@gmail.com"; // Admin's email address
-  $subject = "Annexure Booking Details";
-  $message = "Annexure order has been booked:\n\n";
+  $subject = "Q1-Journal Booking Details";
+  $message = "Q1-Journal order has been booked:\n\n";
   $message .= "Name: $name\n";
   $message .= "Department: $department\n";
   $message .= "Domain: $domain\n";
-  $message .= "Topics: $topics\n";
-  $message .= "To be published in which quartile?: $phd\n";
-  $message .= "Impact Factor: $impact\n";
   $message .= "Email: $email\n";
   $message .= "Mobile Number: $phone\n";
-  $message .= "Do you need assistance?: $assistance\n";
+
 
   // Send email
   if (mail($adminEmail, $subject, $message)) {
